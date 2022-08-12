986ae096cb1e5282b243ae9c3ad5e972754f0c48
R2D2 <jacob.turner870@gmail.com>
2022-08-12 10:32:21 +0200

    [feat]: Moving to tendermint v0.36.x

--- examples/kvstore.rs
+++ examples/kvstore.rs
@@ -38,17 +38,14 @@ impl Service<Request> for KVStore {
             // handled messages
             Request::Info(_) => Response::Info(self.info()),
             Request::Query(query) => Response::Query(self.query(query.data)),
-            Request::DeliverTx(pb::RequestDeliverTx { tx }) => {
-                Response::DeliverTx(self.deliver_tx(tx))
-            }
             Request::Commit(_) => Response::Commit(self.commit()),
+            Request::ProcessProposal(_) => Response::ProcessProposal(Default::default()),
+            Request::FinalizeBlock(_) => Response::FinalizeBlock(Default::default()),
             // unhandled messages
             Request::Echo(_) => Response::Echo(Default::default()),
             Request::Flush(_) => Response::Flush(Default::default()),
             Request::InitChain(_) => Response::InitChain(Default::default()),
-            Request::BeginBlock(_) => Response::BeginBlock(Default::default()),
             Request::CheckTx(_) => Response::CheckTx(Default::default()),
-            Request::EndBlock(_) => Response::EndBlock(Default::default()),
             Request::ListSnapshots(_) => Response::ListSnapshots(Default::default()),
             Request::OfferSnapshot(_) => Response::OfferSnapshot(Default::default()),
             Request::LoadSnapshotChunk(_) => Response::LoadSnapshotChunk(Default::default()),
@@ -105,55 +102,12 @@ impl KVStore {
         }
     }
 
-    fn deliver_tx(&mut self, tx: Vec<u8>) -> pb::ResponseDeliverTx {
-        let tx = String::from_utf8(tx).unwrap();
-        let tx_parts = tx.split('=').collect::<Vec<_>>();
-        let (key, value) = match (tx_parts.get(0), tx_parts.get(1)) {
-            (Some(key), Some(value)) => (*key, *value),
-            _ => (tx.as_ref(), tx.as_ref()),
-        };
-        self.store.insert(key.to_string(), value.to_string());
-
-        // XXX there should be better way to construct responses than this,
-        // but that probably requires nice domain types in tendermint-rs.
-        pb::ResponseDeliverTx {
-            code: 0,
-            data: vec![],
-            log: "".to_string(),
-            info: "".to_string(),
-            gas_wanted: 0,
-            gas_used: 0,
-            events: vec![pb::Event {
-                r#type: "app".to_string(),
-                attributes: vec![
-                    pb::EventAttribute {
-                        key: "key".to_owned(),
-                        value: key.to_owned(),
-                        index: true,
-                    },
-                    pb::EventAttribute {
-                        key: "index_key".to_owned(),
-                        value: "index is working".to_owned(),
-                        index: true,
-                    },
-                    pb::EventAttribute {
-                        key: "noindex_key".to_owned(),
-                        value: "index is working".to_owned(),
-                        index: false,
-                    },
-                ],
-            }],
-            codespace: "".to_string(),
-        }
-    }
-
     fn commit(&mut self) -> pb::ResponseCommit {
         let retain_height = self.height as i64;
         // As in the other kvstore examples, just use store.len() as the "hash"
         self.app_hash = (self.store.len() as u64).to_be_bytes();
         self.height += 1;
         pb::ResponseCommit {
-            data: self.app_hash.to_vec(),
             retain_height,
         }
     }
